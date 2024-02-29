590aceac30acb9048d8d9a5445586db4fe952c8c
mcortesru <>
2024-02-29 13:25:51 +0100

    Hecho según el enuncidado bien

--- src/ARSOFT/ArticuloBundle/Controller/DefaultController.php
+++ src/ARSOFT/ArticuloBundle/Controller/DefaultController.php
@@ -10,4 +10,34 @@ class DefaultController extends Controller
     {
         return $this->render('ARSOFTArticuloBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function listarAction()
+    {
+        $articulos = [
+            ['id' => 1, 'title' => 'Artículo 1', 'created' => '2024-02-01'],
+            ['id' => 2, 'title' => 'Artículo 2', 'created' => '2024-02-02'],
+            ['id' => 3, 'title' => 'Artículo 3', 'created' => '2024-02-03'],
+        ];
+        return $this->render('ARSOFTArticuloBundle:MisVistas:listarArticulos.html.twig', array('articulos' => $articulos));
+    }
+
+    public function showAction($id)
+    {
+        $articulos = [
+            ['id' => 1, 'title' => 'Artículo 1', 'created' => '2024-02-01'],
+            ['id' => 2, 'title' => 'Artículo 2', 'created' => '2024-02-02'],
+            ['id' => 3, 'title' => 'Artículo 3', 'created' => '2024-02-03'],
+        ];
+
+        // Simular la búsqueda de un artículo por ID
+        $articulo = null;
+        foreach ($articulos as $item) {
+            if ($item['id'] == $id) {
+                $articulo = $item;
+                break;
+            }
+        }
+
+        return $this->render('ARSOFTArticuloBundle:MisVistas:show.html.twig', array('articulo' => $articulo));
+    }
 }
